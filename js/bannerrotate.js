de9642ee043cdcfb98967ef1d80a9558a58417f6
MT <minhtrietdiep@gmail.com>
2014-10-13 12:12:08 +0200

    Added random banner on reload

---
+++ js/bannerrotate.js
@@ -0,0 +1,4 @@
+var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
+var banner = banner[Math.floor(Math.random() * banner.length)];
+////document.querySelector('#banner').classList.add(banner);
+document.getElementById('banner').classList.add(banner);
