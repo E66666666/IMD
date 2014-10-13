cbc160269efc2fecd7ec17c4293c1e06fa3df08d
MT <minhtrietdiep@gmail.com>
2014-10-13 12:52:35 +0200

    kjfhdskjfh

--- js/bannerrotate.js
+++ js/bannerrotate.js
@@ -1,9 +1,8 @@
-window.onload = function () { 
+//window.onload = function () { 
 	var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
 	var banner = banner[Math.floor(Math.random() * banner.length)];
 	document.querySelector('#banner').classList.add(banner);
-	window.alert("test");
-}
+//}
 
 //document.getElementById('banner').classList.add(banner);
 //window.alert("test");
