8dcd203877d67ee9ccb2c972258d28e5b6bec661
MT <minhtrietdiep@gmail.com>
2014-10-13 13:42:11 +0200

    Rotating banner on all pages

--- js/bannerrotate.js
+++ js/bannerrotate.js
@@ -1,3 +1,6 @@
+/*
+	JavaScript to load random banners. Banner paths in /style/generic.css
+*/
 var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
 var banner = banner[Math.floor(Math.random() * banner.length)];
 document.querySelector('#banner').classList.add(banner);
