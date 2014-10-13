4354f9b0d269db02c05ce8a829946a7ba13a5a49
MT <minhtrietdiep@gmail.com>
2014-10-13 12:19:58 +0200

    js pls werk
    pls

--- js/bannerrotate.js
+++ js/bannerrotate.js
@@ -1,5 +1,5 @@
 var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
 var banner = banner[Math.floor(Math.random() * banner.length)];
-////document.querySelector('#banner').classList.add(banner);
-document.getElementById('banner').classList.add(banner);
-window.alert("test");
+document.querySelector('#banner').classList.add(banner);
+//document.getElementById('banner').classList.add(banner);
+//window.alert("test");
