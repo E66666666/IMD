62eb360dddfef9a9949ef06d87615f50256c36ef
MT <minhtrietdiep@gmail.com>
2014-10-13 12:38:12 +0200

    window onload in js

--- js/bannerrotate.js
+++ js/bannerrotate.js
@@ -1,5 +1,8 @@
-var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
-var banner = banner[Math.floor(Math.random() * banner.length)];
-document.querySelector('#banner').classList.add(banner);
+window.onload = function () { 
+	var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
+	var banner = banner[Math.floor(Math.random() * banner.length)];
+	document.querySelector('#banner').classList.add(banner);
+}
+
 //document.getElementById('banner').classList.add(banner);
 //window.alert("test");
