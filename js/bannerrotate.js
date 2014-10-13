025ce1107b235b5a4780b73352fd175fb7a02a0e
MT <minhtrietdiep@gmail.com>
2014-10-13 12:40:29 +0200

    all these pushes
    Added a test alert.

--- js/bannerrotate.js
+++ js/bannerrotate.js
@@ -2,6 +2,7 @@ window.onload = function () {
 	var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
 	var banner = banner[Math.floor(Math.random() * banner.length)];
 	document.querySelector('#banner').classList.add(banner);
+	window.alert("test");
 }
 
 //document.getElementById('banner').classList.add(banner);
