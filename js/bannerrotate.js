ac5e0f2274f06783527b9a45a016fa60de3532f1
MT <minhtrietdiep@gmail.com>
2014-10-24 21:34:03 +0200

    Sticky menu bar, responsive sticky menu bar
    Pure JS+CSS solution and none of that jQuery crap.

--- js/bannerrotate.js
+++ js/bannerrotate.js
@@ -4,3 +4,15 @@
 var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
 var banner = banner[Math.floor(Math.random() * banner.length)];
 document.querySelector('#banner').classList.add(banner);
+
+// stick menu to top
+// initial offset
+window.onscroll = function OnScroll() {
+	var offset = 100;
+	var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+	if ( scrollTop >= offset) {
+		document.querySelector('nav').classList.add('stickToTop');
+	} else {
+		document.querySelector('nav').classList.remove('stickToTop');
+	}
+}
