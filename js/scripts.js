2ffbe87d04d3ca7501e0e9888f59d8a6e5395ef4
MT <minhtrietdiep@gmail.com>
2014-10-25 00:53:49 +0200

    Comments

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,11 @@ var banner = [ 'banner1', 'banner2', 'banner3', 'banner4' ];
 var banner = banner[Math.floor(Math.random() * banner.length)];
 document.querySelector('#banner').classList.add(banner);
 
-// stick menu to top
-// initial offset
+/*
+	Sticky menubar
+*/
 window.onscroll = function OnScroll() {
-	var offset = 100;
+	var offset = 100; //height of header
 	var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
 	if ( scrollTop >= offset) {
 		document.querySelector('nav').classList.add('stickToTop');
